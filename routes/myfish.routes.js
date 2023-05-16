65e2c816dd745936596a4a0226ef7bff9dd28df0
kakoff17 <carloskako2@gmail.com>
2023-05-16 13:06:01 +0200

    need alex

--- routes/myfish.routes.js
+++ routes/myfish.routes.js
@@ -8,11 +8,14 @@ const Bowl = require("../models/Bowl.model.js");
 const { isLogged } = require("../middlewares/auth.middlewares.js");
 
 router.get("/:bowlId/create", isLogged, async (req, res, next) => {
-  console.log(req.params.bowlId)
+   
+  //console.log(req.params.bowlId)
   try {
+    const bowl = await Bowl.findById(req.params.bowlId);   
     const allFish = await Fish.find();
     res.render("myfish/create.hbs", {
       allFish,
+      bowl
     });
   } catch (error) {
     next(error);
@@ -21,14 +24,20 @@ router.get("/:bowlId/create", isLogged, async (req, res, next) => {
 
 router.post("/:bowlId/create", isLogged, async (req, res, next) => {
   const { name, age, fish } = req.body;
+  const userId = req.session.activeUser._id;
   console.log(req.body);
   try {
+    const bowl = await Bowl.findById(req.params.bowlId);
+    const fish = await Fish.findOne({
+      name
+    })
     await MyFish.create({
       name,
       age,
-      fish,      
-    }
-    );
+      fish, 
+      bowl, 
+      owner: userId
+    }); res.redirect("/bowl/main")
   } catch (error) {
     next(error);
   }
